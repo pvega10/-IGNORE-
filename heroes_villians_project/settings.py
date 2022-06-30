b9b686ea309c83e1866f803cd1fa4981cfcab388
Paul M Vega <Paul@macbook-pro-2.lan>
2022-06-30 15:26:54 -0500

    redid files from scratch, will update as i go along

--- heroes_villians_project/settings.py
+++ heroes_villians_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-194abl7)kqr3!#v%(f-6**+2^cpx#d^&ktfaapp=asks2s(m(9'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,12 +72,7 @@ WSGI_APPLICATION = 'heroes_villians_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
